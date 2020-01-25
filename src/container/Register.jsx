e1405ac10283ca21c20f1951eceee49b121c4713
gelvezz23 <gelvezz223@gmail.com>
2020-01-25 01:36:07 -0500

    add register form without redux

--- src/container/Register.jsx
+++ src/container/Register.jsx
@@ -1,23 +1,62 @@
-import React from 'react';
+import React,{useState} from 'react';
+//import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Register.scss';
 
 
-const Register = () => (
-    <section className="register">
-        <section className="register__container">
-            <h2>Registrate</h2>
-            <form className="register__container--form">
-                <input className="input--register" type="text" placeholder="Nombre"/>
-                <input className="input--register" type="text" placeholder="Correo"/>
-                <input className="input--register" type="password" placeholder="Contraseña"/>
-                <button className="button"> Registrarme</button>
-            </form>
-            <Link to="/login">Iniciar sesión</Link>
+const Register = (props) => {
+    
+    const [form, setValues] = useState({
+        email:'',
+        name:'',
+        password:'',
+    });
+
+    const handleInput = (event) =>{
+        setValues({
+             ...form,
+             [event.target.name]: event.target.value,   
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form)
+    }
+
+    return(
+        <section className="register">
+            <section className="register__container">
+                <h2>Registrate</h2>
+                <form className="register__container--form" onSubmit={handleSubmit}>
+                    <input 
+                        name="name"
+                        className="input--register" 
+                        type="text" 
+                        placeholder="Nombre"
+                        onChange={handleInput}
+                    />
+                    <input
+                        name="email" 
+                        className="input--register" 
+                        type="text" 
+                        placeholder="Correo"
+                        onChange={handleInput}
+                    />
+                    <input 
+                        name="password"
+                        className="input--register" 
+                        type="password" 
+                        placeholder="Contraseña"
+                        onChange={handleInput}
+                    />
+                    <button className="button"> Registrarme</button>
+                </form>
+                <Link to="/login">Iniciar sesión</Link>
+            </section>
         </section>
-    </section>
-
-);
-
+    );
+}
 export default Register;
